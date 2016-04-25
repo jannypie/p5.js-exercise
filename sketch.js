4371f16e0beebdb69becfd098cf6979ad184e211
Jan Dennison <janice.dennison@inventivhealth.com>
2016-04-25 14:22:52 -0400

    first sketch from tut

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  console.log('Hi from setup');
+  // Create a canvas 200px wide and 200px tall
+    createCanvas(200, 200);
+  // Add a background color
+    background('tomato');
+  // Change framerate
+    frameRate(5);
+}
+
+function draw() {
+  console.log('Hi from draw');
+  // Offset the canvas
+    // translate(10, 10);
+    // random(0, width) returns a value between
+    // 0 and the width of the canvas.
+    // As does random(0, height) for height.
+    translate(random(0, width), random(0, height));
+
+  // Define fill color before shape
+    fill('papayawhip');
+  // Draw a rectangle
+    rect(0, 0, 50, 50);
+  // Define a new fill color
+    fill('thistle');
+  // Add a circle
+    ellipse(25, 25, 25, 25);
+}
